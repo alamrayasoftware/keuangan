e9615c33983a04cdc28c456fcf4f4d5e5f1e2073
Rizqon <m.rum.rowi.r@gmail.com>
2021-03-04 13:25:22 +0700

    add service-provider

---
+++ src/KeuanganServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ArsoftModules\Keuangan;
+
+use Illuminate\Support\ServiceProvider;
+
+class KeuanganServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
